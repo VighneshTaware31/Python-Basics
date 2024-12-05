e3f74e77f25130abf2eeef02e508e0812c62ccb6
VighneshTaware31 <vighneshtaware90@gmail.com>
2024-12-05 13:34:01 +0530

    Add files via upload

---
+++ conditionlogic.py
@@ -0,0 +1,13 @@
+is_old = False
+is_licenced = True
+
+if is_old and is_licenced:
+    print(" you are old enough, and you can drive")
+
+#elif is_licenced:
+    #print("You can drive")
+
+else:
+    print("You cannot drive")
+ 
+  
